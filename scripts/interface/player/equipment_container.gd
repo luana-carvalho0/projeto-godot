bd7652e2b24f725756746fd90c3f902e9893403b
gustavo1902 <gustavomorais1@09gmail.com>
2024-09-14 14:18:58 -0300

    feat: Script para receber o item equipado

--- scripts/interface/player/equipment_container.gd
+++ scripts/interface/player/equipment_container.gd
@@ -7,5 +7,11 @@ onready var consumable_container: TextureRect = get_node("ConsumableBackground")
 onready var armor_container: TextureRect = get_node("VContainer/ArmorBackground")
 onready var weapon_container: TextureRect = get_node("VContainer/WeaponBackground")
 
+func consumable_slot(item_texture: StreamTexture, item_info: Array) -> void:
+	consumable_container.update_consumable_slot(item_texture, item_info)
+	
+func amor_slot(item_texture: StreamTexture, item_info: Array) -> void:
+	armor_container.update_amor_slot(item_texture, item_info)
 
-
+func weapon_slot(item_texture: StreamTexture, item_info: Array) -> void:
+	weapon_container.update_weapon_slot(item_texture, item_info)
