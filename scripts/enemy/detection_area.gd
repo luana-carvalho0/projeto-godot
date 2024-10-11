8cc68e03bdc7249a4142ae686e4d6b768e9978a6
gustavo1902 <gustavomorais1@09gmail.com>
2024-10-11 19:10:16 -0300

    fix: detection_area e script variables em EnemyTemplate

--- scripts/enemy/detection_area.gd
+++ scripts/enemy/detection_area.gd
@@ -5,6 +5,7 @@ export(NodePath) onready var enemy = get_node(enemy) as KinematicBody2D
 
 func _on_body_entered(body: Player) -> void:
 	enemy.player_ref = body
+	print("Player")
 	
 func _on_body_exited(_body: Player) -> void:
 	enemy.player_ref = null
