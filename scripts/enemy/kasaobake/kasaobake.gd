3a813f63d7a351fef2bab87b04241dd319fe4d0b
gustavo1902 <gustavomorais1@09gmail.com>
2024-10-25 13:01:37 -0300

    feat: drop item kasaobake

--- scripts/enemy/kasaobake/kasaobake.gd
+++ scripts/enemy/kasaobake/kasaobake.gd
@@ -1,3 +1,22 @@
 extends EnemyTemplate
 class_name KasaObake
 
+func _ready()-> void:
+	randomize()
+	drop_list = {
+		"Heal Potion ":[
+			"res://assets/item/consumable/health_potion.png",
+			25,
+			"Health",
+			5,
+			2
+		],
+		
+		"Mana Potion": [
+			"res://assets/item/consumable/mana_potion.png",
+			30,
+			"Mana",
+			5,
+			5
+		]
+	}
