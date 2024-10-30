d2aaf04de4daab007a2bbbba94ebe82d53ba19d4
gustavo1902 <gustavomorais1@09gmail.com>
2024-10-30 14:22:17 -0300

    feat: barra de vida para o inimigo

--- scripts/enemy/collision_area.gd
+++ scripts/enemy/collision_area.gd
@@ -7,10 +7,13 @@ export(int) var health
 export(float) var invulnerability_timer
 export(NodePath) var enemy_path  # Caminho do nó do inimigo, para ser configurado no editor
 onready var enemy: KinematicBody2D = null  # Inicialize como nulo
+export(NodePath) onready var enemy_bar = get_node(enemy_bar) as Control
 
 func _ready():
 	if enemy_path != null:
-		enemy = get_node(enemy_path) as KinematicBody2D  # Obtenha o nó do inimigo quando a cena estiver pronta
+		enemy = get_node(enemy_path) as KinematicBody2D
+	if enemy_bar != null:
+		enemy_bar.init_bar(health)
 
 func on_collision_area_entered(area):
 	if area.get_parent() is Player:
@@ -21,6 +24,7 @@ func on_collision_area_entered(area):
 
 func update_health(damage: int) -> void:
 	health -= damage
+	enemy_bar.update_bar(health)
 	enemy.spawn_floating_text("-", "Damage", damage)
 	if health <= 0:
 		enemy.can_die = true
