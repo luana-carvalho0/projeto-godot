85b9380bc1b9a4452ef749f52dce7b5845dcecac
Luana Jesus Carvalho <ljesusca@emeal.nttdata.com>
2024-11-01 14:42:22 -0300

    Finalizando a tela inicial e inciando a tela de seleção de skins

---
+++ scripts/interface/initial_screen.gd
@@ -0,0 +1,62 @@
+extends Control
+
+
+onready var menu: Control = get_node("Menu")
+onready var button_container: VBoxContainer = menu.get_node("ButtonContainer")
+onready var continue_button: Button = button_container.get_node("Continue")
+
+onready var skin_select: Control = get_node("SkinSelect")
+
+func _ready()-> void:
+	for button in get_tree().get_nodes_in_group("button"):
+		button.connect("pressed", self, "on_button_pressed", [button.name])
+		button.connect("mouse_exited", self, "mouse_interection", [button, "exited"])
+		button.connect("mouse_entered", self, "mouse_interection", [button, "entered"])
+	
+	has_save()
+	
+func has_save() -> void:
+	var file: File = File.new()
+	if file.file_exists("data_management.save_path"):
+		continue_button.disabled = false
+		return
+		
+	continue_button.modulate.a = 0.5
+		
+		
+func on_button_pressed(button_name: String)-> void:
+	match button_name:
+		"Play":
+			button_container.hide()
+			skin_select.show()
+		
+		"Continue":
+			var _change_scene: bool = get_tree().change_scene("res://scenes/management/level.tscn")
+		
+		"Quit":
+			get_tree().quit()
+			
+		"BackButton":
+			skin_select.hide()
+			button_container.show()
+			print(button_container)
+			
+	reset()
+		
+		
+func mouse_interection(button: Button, type: String) -> void:
+	if button.disabled:
+		return
+		
+	match type:
+		"exited":
+			button.modulate.a = 1.0
+			
+		"entered":
+			button.modulate.a = 0.5 
+
+func reset() -> void:
+	for button in get_tree().get_nodes_in_group("button"):
+		mouse_interection(button, "exited")
+		
+	has_save()
---
+++ scripts/management/data_management.gd
@@ -0,0 +1,43 @@
+extends Node
+
+var save_path: String = "user://save.dat"
+
+var data_dictionary = {
+	"level": 1,
+	"player_gold": 0,
+	"current_exp": 0,
+	"stats_points": 0,
+	
+	"current_mana": 0,
+	"current_health": 0,
+	
+	"inventory": [],
+	"aux_inventory": [],
+	
+	"base_stats": [],
+	"armor_container": [],
+	"weapon_container": [],
+	"consumable_container": [],
+	
+	"sign_checkpoint": false,
+	"checkpoint": false,
+	"player_texture": "",
+	"player_position": Vector2(-882, 132),
+	"current_level_path": ""
+}
+
+#func save_data() -> void:
+	#var file: File = File.new()
+	#var error = file.open(save_path, File.WRITE)
+	#if error == OK:
+	#	file.store_var(data_dictionary)
+	#	file.close()
+		
+		
+#func load_data() -> void:
+	#var file: File = File.new()
+	#if file.file_exists(save_path):
+	#	var error = file.open(save_path, File.READ)
+	#	if error == OK:
+	#		data_dictionary = file.get_var()
+	#		file.close()
