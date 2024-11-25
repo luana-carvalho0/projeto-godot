2e75fac40eb46c03344f84c3e162d9e40db0822f
Gustavo M. Costa <gustavomorais1009@gmail.com>
2024-11-24 23:25:47 -0300

    feat: reiniciando o jogo após queda

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -138,7 +138,7 @@ func verify_height() -> void:
 		
 		#yield(timer, "timeout")
 		#get_tree().change_scene()
-		get_tree().change_scene("res://scenes/management/level.tscn")
+		get_tree().change_scene("res://scenes/interface/initial_screen.tscn")
 		
 	print(position.y)
 	
